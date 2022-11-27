5e1b66cd1404f6062f3d4aeed1202eb749d692db
bizarlab <110734373+bizarlab@users.noreply.github.com>
2022-11-27 12:25:40 +0100

    Update compute.tf

--- recetas/windows/compute.tf
+++ recetas/windows/compute.tf
@@ -1,26 +1,57 @@
 resource "oci_core_instance" "windows_instance" {
 
     availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-    compartment_id = "Introducir el OCID del compartment donde lo vamos a instalar"
+    compartment_id = "Compartment donde lo vamos a instalar"
     shape = "VM.Standard.E4.Flex"
     shape_config {
-    ocpus = 1
-    memory_in_gbs = 15
+    ocpus = 2
+    memory_in_gbs = 16
+    #admin_username = var.admin_username
+    #admin_password = var.admin_username
     }
-
     source_details {
-
-        source_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaoqdedtw4dsmnk735ibcxu53lb63dec5wknszysi5g5pqfe4slkla"
+        #source_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaoffnm7opezqbhzln3u4lzv6ujteag5h7oxcsio3kr35sp7mlamcq"
+        #source_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaandftbicox5dje7ufgporxov4o3wckbu5mxw27tyjxolekjwrcgsq"
+        source_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa77j4rsjiu4m3ijk762dhaxqs65o2wy6jzxs3circiplykbjtzo2q"
         source_type = "image"
         boot_volume_size_in_gbs = 100
     }
 
-    display_name = "middlewarespyro"
+    display_name = "spyro12c"
     create_vnic_details {
+
         assign_public_ip = false
-        subnet_id = "ocid1.subnet.oc1.eu-frankfurt-1.aaaaaaaa2cxb5r5ebdktaeyxvrovwu5abx2jhuymxi5v5yicq4odjruprbzq"
+        subnet_id = "OCID SUBNET"
     }
-
+    extended_metadata = {
+        ssh_authorized_keys = file("/home/oracle/.ssh/id_rsa_terraform.pub")
+    }
     preserve_boot_volume = false
+}
+resource "oci_core_volume" "CreateVolume"{
 
+        availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+        compartment_id = "COMPARTMENT OCID"
+        display_name = "oradata"
+        size_in_gbs = 100
+}
+
+resource "oci_core_volume_attachment" "CreateVolumeAttachment"{
+
+        attachment_type = "iscsi"
+        instance_id = oci_core_instance.windows_instance.id
+        volume_id = oci_core_volume.CreateVolume.id
+}
+resource "oci_core_volume" "CreateVolume2"{
+
+        availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+        compartment_id = "compartment ocid"
+        display_name = "backup"
+        size_in_gbs = 100
+}
+resource "oci_core_volume_attachment" "CreateVolumeAttachment2"{
+
+        attachment_type = "iscsi"
+        instance_id = oci_core_instance.windows_instance.id
+        volume_id = oci_core_volume.CreateVolume2.id
 }
